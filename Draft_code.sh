500d57049e26f2603498c07198f870f9b9edc5ef
kateburley <153732803+kateburley@users.noreply.github.com>
2024-10-17 12:13:19 +0100

    Update Draft_code.sh

--- Draft_code.sh
+++ Draft_code.sh
@@ -43,9 +43,9 @@ then
 ${gcta} \
 	--bfile ${bfile} \
 	--mlma-loco \
-	--pheno ${home_directory}/processed_data/methylation_data/all_covariates.txt \
-	--qcovar ${home_directory}/processed_data/methylation_data/all_covariates.txt \
-	--covar ${home_directory}/processed_data/methylation_data/all_covariates.txt \
+	--pheno ${home_directory}/processed_data/methylation_data/all_covariates.plink \
+	--qcovar ${home_directory}/processed_data/covariate_data/covariates_intersectids.txt \
+	--covar ${home_directory}/processed_data/covariate_data/covariates_intersectids.txt \
 	--autosome \
 	--out ${home_directory}/results/12/cellcount_${batch} \
 	--thread-num ${nthreads} \
