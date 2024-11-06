ae378d7735456be724ed91ea6831f8ce1560dc4f
kateburley <153732803+kateburley@users.noreply.github.com>
2024-11-06 14:50:42 +0000

    Update Draft_code.sh

--- Draft_code.sh
+++ Draft_code.sh
@@ -150,18 +150,17 @@ fi
 echo "Compressing results"
 gzip -f ${section_12_dir}/cellcount_${batch}.loco.mlma
 
-
+echo "Making plots"
 Rscript resources/genetics/plot_gwas.R \
 	${section_12_dir}/cellcount_${batch}.loco.mlma.gz \
 	9 \
 	1 \
 	3 \
+ 	2 \
 	TRUE \
 	0 \
 	0 \
 	0 \
 	0 \
-	${section_12_dir}/cellcount_${batch}.loco.mlma
-
 
 echo "Successfully performed GWAS for cell type ${batch}"
